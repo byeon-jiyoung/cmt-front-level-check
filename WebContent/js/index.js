82eaf5311aa4943c9b32d6bf31f7a993a00966d6
byeonjiyoung <bjy8668@hanmail.net>
2019-11-05 18:28:29 +0900

    js수정 및 반응형 진행 중

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,18 +1,50 @@
 /**
  * 
  */
+function displayNone(style) {
+	document.getElementById("fName").style.display = style;
+	document.getElementById("lName").style.display = style;
+	document.getElementById("uName").style.display = style;
+	document.getElementById("country").style.display = style;
+	document.getElementById("state").style.display = style;
+	document.getElementById("zip").style.display = style;
+	document.getElementById("check").style.display = style;
+	document.getElementById("noc").style.display = style;
+	document.getElementById("ccn").style.display = style;
+	document.getElementById("exp").style.display = style;
+	document.getElementById("cvv").style.display = style;
+	for(var i=0; i<document.getElementsByClassName("pass").length; i++) {
+		document.getElementsByClassName("pass")[i].style.borderColor="#28a745";
+		document.getElementsByClassName("pass")[i].style.color="#28a745";
+	}
+}
+
+function resetForm() {
+	document.baForm.fName.value = "";
+	document.baForm.lName.value = "";
+	document.baForm.uName.value = "";
+	document.baForm.email.value = "";
+	document.baForm.address.value = "";
+	document.baForm.address2.value = "";
+	document.baForm.country.selectedIndex = 0; //select
+	document.baForm.state.selectedIndex = 0; //select
+	document.baForm.zip.value = "";
+	document.baForm.noc.value = "";
+	document.baForm.ccn.value = "";
+	document.baForm.exp.value = "";
+	document.baForm.cvv.value = "";
+	for(var i=0; i<document.baForm.check.length; i++) { //check
+		document.baForm.check[i].checked == false;
+	}
+}
+
+
 function formCheck() {
-	document.getElementById("fName").style.display = "none";
-	document.getElementById("lName").style.display = "none";
-	document.getElementById("uName").style.display = "none";
+	var check = true;
+	
 	document.getElementById("email").style.display = "none";
-	document.getElementById("country").style.display = "none";
-	document.getElementById("state").style.display = "none";
-	document.getElementById("zip").style.display = "none";
-	document.getElementById("noc").style.display = "none";
-	document.getElementById("ccn").style.display = "none";
-	document.getElementById("exp").style.display = "none";
-	document.getElementById("cvv").style.display = "none";
+	
+	displayNone("none");
 	
 	var fName = document.baForm.fName.value;
 	var lName = document.baForm.lName.value;
@@ -36,22 +68,24 @@ function formCheck() {
 	var exp = document.baForm.exp.value;
 	var cvv = document.baForm.cvv.value;
 	
+	
 	//First name 입력하지 않은 경우
 	if(!fName) {
 		document.getElementById("fName").style.display = "block";
-		return false;
+		document.baForm.fName.focus();
+		check = false;
 	}
 	
 	//Last name 입력하지 않은 경우
 	if(!lName) {
 		document.getElementById("lName").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//Username 입력하지 않은 경우
 	if(!uName) {
 		document.getElementById("uName").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//email형식이 틀린 경우
@@ -60,55 +94,62 @@ function formCheck() {
 		var result = emailReg.test(email);
 		if(result == false) {
 			document.getElementById("email").style.display = "block";
-			return false;
+			check = false;
 		}
 	}
 	
 	//Country 선택하지 않은 경우
 	if(country == 0) {
 		document.getElementById("country").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//State 선택하지 않은 경우
 	if(state == 0) {
 		document.getElementById("state").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//Zip 입력하지 않은 경우
 	if(!zip) {
 		document.getElementById("zip").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//체크박스 선택하지 않은 경우
 	if(checkCount == 0) {
 		document.getElementById("check").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//Name on card 입력하지 않은 경우
 	if(!noc) {
 		document.getElementById("noc").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//Credit card number 입력하지 않은 경우
 	if(!ccn) {
 		document.getElementById("ccn").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//Expiration 입력하지 않은 경우
 	if(!exp) {
 		document.getElementById("exp").style.display = "block";
-		return false;
+		check = false;
 	}
 	
 	//CVV 입력하지 않은 경우
 	if(!cvv) {
 		document.getElementById("cvv").style.display = "block";
+		check = false;
+	}
+	
+	if(check == false) {
 		return false;
+	}else {
+		resetForm();
+		return true;
 	}
 }
